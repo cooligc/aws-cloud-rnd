55574e72bc2a979ad542cffd3a1cd06eca79ca18
sitakant1991 <sitakant1991@gmail.com>
2015-07-16 16:57:30 +0530

    Creating bucket, creating a file on the bucket, downloading a file ,deleting a file, Getting the meta-data of S3

--- AwsProjects/S3Implementation/src/main/java/org/skc/app/App.java
+++ AwsProjects/S3Implementation/src/main/java/org/skc/app/App.java
@@ -1,7 +1,14 @@
 package org.skc.app;
 
-import com.amazonaws.services.s3.AmazonS3;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 import org.skc.util.S3Utils;
+
+import com.amazonaws.services.s3.AmazonS3;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,8 +18,24 @@ import org.skc.util.S3Utils;
  * To change this template use File | Settings | File Templates.
  */
 public class App {
-    public static void main(String... strings){
+    public static void main(String... strings) throws IOException{
+    	final String bucketName="sample-igc-docbase";
         AmazonS3 amazonS3 = S3Utils.createS3Client(null);
-        S3Utils.createBucket(amazonS3,"dummyBucket");
+        S3Utils.createBucket(amazonS3,bucketName);
+        File file = createFile();
+        S3Utils.createFileOnS3(amazonS3, bucketName, file);
+        S3Utils.downloadFileFromS3(amazonS3, bucketName, file.getName());
+        S3Utils.deleteFileOnS3(amazonS3, bucketName, file);
+        S3Utils.readMetaDataForS3(amazonS3);
+        
     }
+
+	private static File createFile() throws FileNotFoundException, UnsupportedEncodingException {
+		String fileName="skc"+System.currentTimeMillis();
+		PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+		writer.println("The first line");
+		writer.println("The second line");
+		writer.close();
+		return new File(fileName);
+	}
 }
